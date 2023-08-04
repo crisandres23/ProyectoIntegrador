b6eb0c90bc19f23c26d361864b043ab6b9f2664d
Andres <xndres2301@gmail.com>
2023-08-04 14:39:55 -0500

    master

--- ProyectoIntegrador.py
+++ ProyectoIntegrador.py
@@ -3,3 +3,29 @@
 nombre = input("Escriba su nombre: ")
 apellido = input(f"Escriba su apellido, {nombre}: ")
 print(f"Bienvenido, {nombre} {apellido}.")
+
+#Proyecto integrador parte 2
+print ("EMPEZEMOS!!!")
+
+import readchar
+keypress = readchar.readkey()
+tecla = (input("presione una tecla:"))
+
+while True:
+    # Leer la tecla que se presiona
+    tecla = readchar.readkey()
+    # Imprimir la tecla en la pantalla
+    print(tecla)
+    # Comprobar si la tecla es la flecha hacia arriba
+    if tecla == readchar.key.UP:
+        # Asignar el valor de la tecla ENTER a la variable keypress
+        keypress = readchar.key.UP
+        # Salir del bucle
+        break
+    # Comprobar si la tecla es UP
+    if tecla == readchar.key.UP:
+        # Salir del bucle
+        break
+
+
+
