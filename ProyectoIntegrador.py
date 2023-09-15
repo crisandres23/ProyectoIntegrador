e2d1f895ffb2ca91cf7f1184b412a7cb994347f5
Andres <xndres2301@gmail.com>
2023-09-15 18:21:53 -0500

    aster

--- ProyectoIntegrador.py
+++ ProyectoIntegrador.py
@@ -45,7 +45,100 @@ while numero < 50:
     elif char == readchar.key.UP:
         print("Presionaste la tecla UP. Saliendo del programa")
         break
+#Parte 4
+import os
+import keyboard
 
-print("Fin del programa")
+def terminal():
+    os.system("cls" if os.name == "nt" else "clear")
+    print("\n" * 100)
 
+def limpiar_pantalla(matriz):
+    os.system("cls")
+    for fila in matriz:
+        print("".join(fila))
+
+def convertir_mapa(mapa):
+    mapa = mapa.strip()
+    filas = mapa.split("\n")
+    matriz = [list(fila) for fila in filas]
+    return matriz
+
+def posicion_inicial(laberinto):
+    for i in range(len(laberinto)):
+        for j in range(len(laberinto[i])):
+            if laberinto[i][j] == "P":
+                posicion_inicial = (i, j)
+            elif laberinto[i][j] == "E":
+                posicion_final = (i, j)
+    return posicion_inicial, posicion_final
+
+def limpiar_y_mostrar(matriz, px, py):
+    terminal()
+    for fila in matriz:
+        print("".join(fila))
+    matriz[py][px] = "P"
+
+mapa = """
+####################
+#P...............###
+#.#.##############.#
+#.#..............#.#
+#.#.#..#########.#.#
+#.#.#..........#.#.#
+#.#.#.########.#.#.#
+#.#.#.#......#.#.#.#
+#.#.#.#.####.#.#.#.#
+#...#...#..#...#...#
+#.#####.##.##.#####.
+#.....#....#.....#.#
+#####.######.#####.#
+#.....#....#.....#.#
+#.#####.##.##.#####.
+#...#...#..#...#.###
+#.###.#.####.#.#...#
+#...#.#......#.#.###
+#.###.#.####......E#
+"""
+
+matriz = convertir_mapa(mapa)
+posicion_inicial, posicion_final = posicion_inicial(matriz)
+px, py = posicion_inicial
+
+salir = False
+
+while (px, py) != posicion_final and matriz[py][px] != "#":
+    if keyboard.is_pressed("up"):
+        if py > 0 and matriz[py-1][px] != "#":
+            matriz[py][px] = "."
+            py -= 1
+            matriz[py][px] = "P"
+            posicion_inicial = (px, py)  # Actualizar la posición inicial
+        limpiar_y_mostrar(matriz, px, py)
+    elif keyboard.is_pressed("down"):
+        if py < len(matriz) - 1 and matriz[py+1][px] != "#":
+            matriz[py][px] = "."
+            py += 1
+            matriz[py][px] = "P"
+            posicion_inicial = (px, py)  # Actualizar la posición inicial
+        limpiar_y_mostrar(matriz, px, py)    
+    elif keyboard.is_pressed("left"):
+        if px > 0 and matriz[py][px-1] != "#":
+            matriz[py][px] = "."
+            px -= 1
+            matriz[py][px] = "P"
+            posicion_inicial = (px, py)  # Actualizar la posición inicial
+        limpiar_y_mostrar(matriz, px, py)    
+    elif keyboard.is_pressed("right"):
+        if px < len(matriz[0]) - 1 and matriz[py][px+1] != "#":
+            matriz[py][px] = "."
+            px += 1
+            matriz[py][px] = "P"
+            posicion_inicial = (px, py)  # Actualizar la posición inicial
+        limpiar_y_mostrar(matriz, px, py)
+    
+    if salir:
+            break
+
+print("¡Felicidades! ¡Lograste salir del laberinto!")
 
